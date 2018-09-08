6ed3fe22f9b85296a8ac326d4966124352d50c49
furkan <furkanmumcu2@gmail.com>
2018-09-08 23:40:02 +0300

    Update ListFolderSizes.ps1

--- ListFolderSizes.ps1
+++ ListFolderSizes.ps1
@@ -1,19 +1,22 @@
-Function ListFolderSizes($path){
+Function ListFolderSizes($path, [boolean]$sortBySize){
     
     #find sub directory names
-    $dirs = Get-ChildItem $path | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}
+    $dirs = Get-ChildItem $path -Directory | Foreach-Object {$_.Name}
     
     $list = New-Object System.Collections.Specialized.OrderedDictionary
+
 
     $sizes = @(0) * $dirs.Count
     for ($i = 0; $i -lt $dirs.Count ; $i++) {
         $newpath = $path + "\" + $dirs[$i]   
-        $sizes[$i] = (Get-ChildItem -path $newpath -recurse | Measure-Object -property length -sum ).sum /1MB
-        
+        $sizes[$i] = "{0:N2}" -f ((Get-ChildItem -path $newpath -recurse -File | Measure-Object -property length -sum ).sum /1MB)
         $list.Add($dirs[$i],$sizes[$i])
     }
     
-    #echo $path
-    #echo $sizes[1]
-    echo $list
+    if($sortBySize){
+        $list.GetEnumerator() | sort value
+    }
+    else{
+        $list
+    }
 }
